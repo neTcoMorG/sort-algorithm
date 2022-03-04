e8f9a9d51ae97799e803c79c04f7766734f237dc
Jo young jun <joyoungjun8590@hotmail.com>
2022-03-04 23:14:17 +0900

    first commit

--- src/me/dudwns/Main.java
+++ src/me/dudwns/Main.java
@@ -1,5 +1,36 @@
 package me.dudwns;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        int[] array = new int[] {1, 5, 4, 9, 6, 8, 2, 7, 3, 10};
+        int index = 0, temp;
+
+        for (int i=0; i < array.length; i++) {
+            int min = 999;
+
+            for (int j=i; j < array.length; j++) {
+                if (min > array[j]) {
+                    min = array[j];
+                    index = j;
+                }
+            }
+
+            System.out.print(" index[" + i + "], min " + min + " " + "   Before ");
+            for (int p : array) {
+                System.out.print(p + " ");
+            }
+
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+
+            System.out.print(" After ");
+            for (int p : array) {
+                System.out.print(p + " ");
+            }
+
+            System.out.println();
+        }
+    }
 }
